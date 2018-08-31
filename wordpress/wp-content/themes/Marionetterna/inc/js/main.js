1d7a407c8ebe5701ad191850ce1c60a3d867dcda
Filiph Eriksson-Falk <filiph.erikssonfalk@hiq.se>
2018-09-01 00:14:25 +0200

    Add responsiveness to navigation-menu and leader-page
    Update to latest bootstrap version (4.1.3) using CDN.

--- wordpress/wp-content/themes/Marionetterna/inc/js/main.js
+++ wordpress/wp-content/themes/Marionetterna/inc/js/main.js
@@ -1,1 +1,37 @@
-jQuery(document).ready(function(t){t('#submit, .wpcf7-submit, .comment-reply-link, input[type="submit"]').addClass("btn btn-default"),t(".wp-caption").addClass("thumbnail"),t(".widget_rss ul").addClass("media-list"),t("table#wp-calendar").addClass("table table-striped"),t(window).scroll(function(){t(this).scrollTop()>100?t(".scroll-to-top").fadeIn():t(".scroll-to-top").fadeOut()}),t(".scroll-to-top").click(function(){return t("html, body").animate({scrollTop:0},800),!1}),function(){var t=navigator.userAgent.toLowerCase().indexOf("webkit")>-1,e=navigator.userAgent.toLowerCase().indexOf("opera")>-1,n=navigator.userAgent.toLowerCase().indexOf("msie")>-1;(t||e||n)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e),t&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus()))},!1)}()});jQuery(window).load(function(){jQuery(".flexslider").length&&jQuery(".flexslider").flexslider({animation:"fade",controlNav:!0,prevText:"",nextText:"",smoothHeight:!0})});
+jQuery(document).ready(function($) {
+    $('#submit, .wpcf7-submit, .comment-reply-link, input[type="submit"]').addClass("btn btn-default"), $(".wp-caption").addClass("thumbnail"), $(".widget_rss ul").addClass("media-list"), $("table#wp-calendar").addClass("table table-striped"), $(window).scroll(function() {
+            $(this).scrollTop() > 100 ? $(".scroll-to-top").fadeIn() : $(".scroll-to-top").fadeOut()
+        }), $(".scroll-to-top").click(function() {
+            return $("html, body").animate({
+                scrollTop: 0
+            }, 800), !1
+        }),
+        function() {
+            var t = navigator.userAgent.toLowerCase().indexOf("webkit") > -1,
+                e = navigator.userAgent.toLowerCase().indexOf("opera") > -1,
+                n = navigator.userAgent.toLowerCase().indexOf("msie") > -1;
+            (t || e || n) && document.getElementById && window.addEventListener && window.addEventListener("hashchange", function() {
+                var t, e = location.hash.substring(1);
+                /^[A-z0-9_-]+$/.test(e) && (t = document.getElementById(e), t && (/^(?:a|select|input|button|textarea)$/i.test(t.tagName) || (t.tabIndex = -1), t.focus()))
+            }, !1)
+        }()
+
+        $('#navbar .menu-item-has-children').click(function(e){
+            var target = $(e.target);
+            target.parent(".menu-item-has-children").toggleClass('open');
+        });
+        $('#hamburger-button').click(function(e){
+            e.preventDefault();
+            $(e.currentTarget).toggleClass('is-active');
+            $('#navbar').toggleClass('overlay-active');
+        });
+});
+jQuery(window).load(function() {
+    jQuery(".flexslider").length && jQuery(".flexslider").flexslider({
+        animation: "fade",
+        controlNav: !0,
+        prevText: "",
+        nextText: "",
+        smoothHeight: !0
+    })
+});
