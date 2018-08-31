2f2463e9e7131a54daa72972c66b9b1a4391f9d9
Frenning <fredrikfrenning@gmail.com>
2018-08-31 19:10:23 +0200

    widgets and frontpage
    minor styling changes

--- wordpress/wp-content/themes/Marionetterna/front-page.php
+++ wordpress/wp-content/themes/Marionetterna/front-page.php
@@ -10,6 +10,13 @@
       <h1 class="heading8">
       <?php echo wp_kses_post(get_bloginfo('description')); ?>
       </h1>
+      <div class="post-area col-md-12">
+        <?php 
+          $post_id = 100;
+          $queried_post = get_post($post_id);
+          echo $queried_post->post_content;
+        ?>
+      </div>
       <?php echo do_shortcode("[custom-facebook-feed]"); ?>          
   </div><!--content-inside-->
 </div><!-- #primary -->
